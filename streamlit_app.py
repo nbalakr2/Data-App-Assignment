cb6cbbab1c341e5ed7d3ace4754d06b9815ea51d
nbalakr2 <nbalakr2@charlotte.edu>
2025-08-06 22:36:12 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,15 +19,15 @@ st.bar_chart(df.groupby("Category", as_index=False).sum(), x="Category", y="Sale
 
 # Aggregating by time
 # Here we ensure Order_Date is in datetime format, then set is as an index to our dataframe
-df["Order_Date"] = pd.to_datetime(df["Order_Date"])
-df.set_index('Order_Date', inplace=True)
+#   df["Order_Date"] = pd.to_datetime(df["Order_Date"])
+#   df.set_index('Order_Date', inplace=True)
 # Here the Grouper is using our newly set index to group by Month ('M')
-sales_by_month = df.filter(items=['Sales']).groupby(pd.Grouper(freq='M')).sum()
+#   sales_by_month = df.filter(items=['Sales']).groupby(pd.Grouper(freq='M')).sum()
 
-st.dataframe(sales_by_month)
+#   st.dataframe(sales_by_month)
 
 # Here the grouped months are the index and automatically used for the x axis
-#st.line_chart(sales_by_month, y="Sales")
+# st.line_chart(sales_by_month, y="Sales")
 
 # ✅ Prompt 1: Category Dropdown
 selected_category = st.selectbox(
